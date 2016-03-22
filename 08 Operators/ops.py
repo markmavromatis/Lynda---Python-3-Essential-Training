fcd5874a086c82aba1aef902c41c4f33f1960bdc
Mark Mavromatis <mmavromatis@guidewire.com>
2016-03-22 09:48:28 -0700

    Lesson 8 operators

--- 08 Operators/ops.py
+++ 08 Operators/ops.py
@@ -6,4 +6,14 @@
 def main():
     print("This is the ops.py file.")
 
+    #Operator examples
+    print("5 + 5 = {}".format(5 + 5))
+    print("5 * 5 = {}".format(5 * 5))
+    print("5 - 3 = {}".format(5 - 3))
+    print("5 / 3 = {}".format(5 / 3))
+    print("5 // 3 = {}".format(5 // 3))
+    print("5 % 3 = {}".format(5 % 3))
+    print("divmod(5, 3) = {}".format(divmod(5, 3)))
+
+
 if __name__ == "__main__": main()
