c5c40fb8a2c594db5294f3f296a38c03bb5cc553
Mark Mavromatis <mmavromatis@guidewire.com>
2016-03-22 09:48:16 -0700

    Lesson 7 Loops

--- 07 Loops/for.py
+++ 07 Loops/for.py
@@ -4,8 +4,27 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
+
+    # For loop can use any iterator.
+    # be it file input, containers, strings
     fh = open('lines.txt')
     for line in fh.readlines():
-        print(line)
+        print(line, end = "")
+
+    for each_number in (1,2,3,4,5):
+        print(each_number)
+
+    for each_number in [1,2,3,4,5]:
+        print(each_number)
+
+    for each_character in 'string':
+        print(each_character)
+
+
+    # How about index?
+    for i, a_char in enumerate('this is a string'):
+        if a_char == 's':
+            print("Index {} is an s ".format(i))
+
 
 if __name__ == "__main__": main()
--- 07 Loops/loopcontrol.py
+++ 07 Loops/loopcontrol.py
@@ -5,7 +5,27 @@
 
 def main():
     s = 'this is a string'
+
+    # Print string and skip all s's.
+    for c in s:
+        if c == 's':
+            continue
+        print(c, end='')
+
+    print()
+    # Print string and stop at first s.
+    for c in s:
+        if c == 's':
+            break
+        print(c, end='')
+
+    print()
+
+    # For Loop Else continues after last iterator value.
     for c in s:
         print(c, end='')
+    else:
+        print("else")
+
 
 if __name__ == "__main__": main()
